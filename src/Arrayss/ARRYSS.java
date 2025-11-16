926b51601a1d25bc25aa852e8493eaa4118901ff
halaatef <hagsmeerh@gmail.com>
2025-11-16 12:39:50 +0300

    LAB-3

---
+++ src/Arrayss/ARRYSS.java
@@ -0,0 +1,9 @@
+package Arrayss;
+
+public class ARRYSS {
+    public static class main {
+        public static void main(String[] args) {
+            System.out.println("test 2");
+        }
+    }
+}
